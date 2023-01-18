5ab302e4ae9524bf20cbad498302b1c700615004
Dooha005 <MINGW64@gmail.com~>
2023-01-18 23:14:49 +0300

    SOLVED LAB .

--- lab_solve.py
+++ lab_solve.py
@@ -1,4 +1,4 @@
-user_inputt = input("Do you want to add a new To-Do item? Answer by y for yes and n for no? \n")
+user_inputt = input("Do you want to add a new To-Do item? Answer by y for yes and n for no : \n")
 yes_choices = ['y']
 no_choices = ['n']
 exit_choices = ['exit']
@@ -11,11 +11,11 @@ while user_inputt !='exit':
         user_write = input("Type what you want to do : ")
         file_write.write(user_write + "/n")
         file_write.close()
-        user_inputt = input("Do you want to add a new To-Do item? Aanswer by y for yes and n for no? \n")
+        user_inputt = input("Do you want to add a new To-Do item? Aanswer by y for yes and n for no : \n")
 
     elif user_inputt.lower() in no_choices:
 
-        user_inputt = input("do you want to list your To-Do items ? Answer y for yes and n for no: \n")
+        user_inputt = input("Do you want to list your To-Do items ? Answer y for yes and n for no : \n")
 
         if user_inputt.lower() in yes_choices:
 
@@ -23,7 +23,7 @@ while user_inputt !='exit':
             content = file_read.readlines()
             print("content of to_do.txt is : ", content)
             file_read.close()
-            user_inputt = input("Do you want to add a new To-Do item? Answer by y for yes and n for no? \n")
+            user_inputt = input("Do you want to add a new To-Do item? Answer by y for yes and n for no : \n")
 
     else:
         print("Thank you for using the To-Do program, come back again soon")
