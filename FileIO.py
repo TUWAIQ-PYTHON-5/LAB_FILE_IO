2ef0b3ab91b4a3a10993f47698a4a723d477c10d
halrasheed <haroon.arrsheed@gmail.com>
2023-01-18 21:58:39 +0300

    FilesIO Lab upload

---
+++ FileIO.py
@@ -0,0 +1,21 @@
+
+usrAnswer:str=""
+
+while (usrAnswer != "exit"):
+    print("Do you want to add a new To-Do List?")
+    usrAnswer= input("Answer by ""[y]"" for yes and ""[n]"" for no.")
+    if (usrAnswer == "y"):
+        usrItem=input("Type What you want to do: ")
+        usrFile = open('to_do.txt', "a", encoding="utf-8")
+        usrFile.writelines(usrItem+"\n")
+        usrFile.close()
+
+    elif (usrAnswer == "n"):
+        print("Do you want to list your To-Do items?")
+        follAnswer:str = input("Answer by ""[y]"" for yes and ""[n]"" for no.")
+        if (follAnswer == "y"):
+            usrFile = open('to_do.txt', "r", encoding="utf-8")
+            print(usrFile.read())
+            usrFile.close()
+else: print("Thank you for using the To-Do program, come back again soon")
+
