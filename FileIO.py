de350b4c9caf8ad3d0e675e6a60629223cea1041
halrasheed <haroon.arrsheed@gmail.com>
2023-01-18 21:59:37 +0300

    FilesIO Lab upload

--- FileIO.py
+++ FileIO.py
@@ -3,7 +3,7 @@ usrAnswer:str=""
 
 while (usrAnswer != "exit"):
     print("Do you want to add a new To-Do List?")
-    usrAnswer= input("Answer by ""[y]"" for yes and ""[n]"" for no.")
+    usrAnswer= input("Answer by ""[y]"" for yes and ""[n]"" for no. \n")
     if (usrAnswer == "y"):
         usrItem=input("Type What you want to do: ")
         usrFile = open('to_do.txt', "a", encoding="utf-8")
@@ -12,7 +12,7 @@ while (usrAnswer != "exit"):
 
     elif (usrAnswer == "n"):
         print("Do you want to list your To-Do items?")
-        follAnswer:str = input("Answer by ""[y]"" for yes and ""[n]"" for no.")
+        follAnswer:str = input("Answer by ""[y]"" for yes and ""[n]"" for no.\n")
         if (follAnswer == "y"):
             usrFile = open('to_do.txt', "r", encoding="utf-8")
             print(usrFile.read())
