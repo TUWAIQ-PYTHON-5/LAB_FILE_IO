5a98ef3fbaccfbb15af8e3525a443aa11072d53f
raghad nasser <Ra9g77@gmail.com>
2023-01-19 14:38:38 +0300

    lab files solved

--- lab file.py
+++ lab file.py
@@ -1,33 +1,29 @@
 
-
 #file= open("to_do.txt","w"")
 
-user_aswere=input("Do you want to add a new To-Do item? Enter y for yes and n for no : " )
-
-while user_aswere== "y":
-    file= open("to_do.txt","a")
-    to_do_list=input("TYPE YOUR TO DO ITEM :")  
-    file.writelines(to_do_list+'\n')
-    if to_do_list=="":
-        file.close()
-        print ("thank you for using the To-Do program, come back again soon")
-        break
-        
-   
-       
-
-
-user_aswere2= input("Do you want to list your To-Do items ? answer y for yes and n for no :")
-while user_aswere2=="y":
-     file= open("to_do.txt","r")
-     content=file.readlines()
-     print(content)
-     break
-     if user_aswere2=="n":   
-       user_aswere=input("Do you want to add a new To-Do item? Enter y for yes and n for no : " )
-
+print("welcome to our program to exit type exit:")
+while True:
+  user_aswere=input("Do you want to add a new To-Do item? Enter y for yes and n for no or type exit to exit : " )
+  if user_aswere== "y":
+     to_do_list=input("TYPE YOUR TO DO ITEM :") 
+     file= open("to_do.txt","a")
+     file.writelines(to_do_list+'\n')
+     file.close()
+  elif user_aswere=="no" :     
+   user_aswere_2= input("Do you want to list your To-Do items ? answer y for yes and n for no :")     
+   if user_aswere_2=="y":
+      file= open("to_do.txt","r") 
+      content=file.readlines()
+      for item in content:
+        print(item)
+      file.close()
  
+  elif user_aswere=="exit":
+    break
 
-       
-       
+print ("thank you for using the To-Do program, come back again soon")
+
+
+
+
 
