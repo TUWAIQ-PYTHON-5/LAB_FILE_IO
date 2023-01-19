e25f1adb86aa971e149d0c1326db321eec342ccf
sultan <sultan_33_886@outlook.sa>
2023-01-19 08:22:01 +0300

    lab_LAB_file

---
+++ lab file.py
@@ -0,0 +1,22 @@
+qustion=input("do you want to add a new To-Do item  :")
+while qustion =="y":
+
+ file = open("to_do.txt", "w", encoding="utf-8")
+ word=file.write("i love my city")
+ file.close()
+ print(input("what the type of new To-Do:"))
+else:
+ while qustion=="n":
+    print(input("do you want to list your To-Do items :"))
+while qustion =="y":
+    print(file.write("i\n love\n my\n city"))
+    break
+qustion=input("do you want to add a new To-Do item  :")
+print(exit)
+
+
+ 
+ 
+
+
+
