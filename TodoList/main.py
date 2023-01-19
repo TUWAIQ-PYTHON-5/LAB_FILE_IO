c73f59c0d5dd7f82ddc37198286bfd65bf01e63f
Ahmed Alhisan <alhisan.swe@gmail.com>
2023-01-19 09:50:42 +0300

    edit

--- TodoList/main.py
+++ TodoList/main.py
@@ -1,41 +1,29 @@
-import os
-import shutil
+flag = True
+while flag:
 
+        show_msg =  input("do you want to add a new To-Do item ? y / n  or exit to close : ")
+        if show_msg == "y":
+            print("-------------------------")
+            new_to_do = input("type in new To-Do item : ")
+            file = open('to_do.txt', 'a+' , encoding='utf-8')
+            file.write(new_to_do+'\n')
+            file.close()
 
-user_awnser = ""
+        elif show_msg == "n":
+             print("-------------------------")
+             show_list = input("do you want to list your To-Do items ?  : ")
+             print("-------------------------")
+             if show_list == 'y':
+                files = open('to_do.txt' , 'r', encoding='utf-8') 
+                print(files.read())
+                print("-------------------------")
+                files.close()
 
-
-   
-
-
-    
-while user_awnser != "exit":
-    user_awnser = input("do you want to add a new To-Do item ? y for yes : n for No : ")
-    if user_awnser == "y":
-        number_of_task = int(input("please enter number of task that you want add : ?"))
-        for i in range(0 , number_of_task):
-            file_name =open('example.txt'  , "a+" , encoding="utf-8") 
-            file_name.writelines([ input("please enter your task" "\n") , "   TASK YOU HAVE TO DO""\n" ])
-            file_name.close()
-
-
-    elif user_awnser == "n":
-        show_user_task= input("do you want to list your To-Do items ? : ")
-
-        if show_user_task == "y":
-            file_name = open("example.txt", "r", encoding="utf-8")
-            content = file_name.read()
-            print("your do lis is :" "\n" , content)
-            file_name.close()
-
-        elif show_user_task == "n":
-            print("thank you for using the To-Do program, come back again soon")   
-            break       
-else:
-
-    print("thank you for using the To-Do program, come back again soon")
-
-             
+        elif show_msg == "exit":
+                flag = False
+                print("thank you for using the To-Do program, come back again soon")
+                print("-------------------------")
+           
 
        
     
