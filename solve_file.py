c294645bba580ac94f4431a02287e74046f02e2d
asrar <asrar.rifae@hotmail.com>
2023-01-19 10:42:54 +0300

    solve LAB_FILE_IO lab

--- solve_file.py
+++ solve_file.py
@@ -1,12 +1,49 @@
 
+while True :
+    user_answers = input("Do you want to add a new To-Do item? (y/n) or exit to end:  ")
+    if user_answers == "y":
+        todo_item  = input(" To-Do item?:  ")
+        file = open("to_do.txt","a",encoding="utf-8")
+        file.write(todo_item  + "\n")
+        file.close()
+    elif user_answers == "n" :
+        user_sub_answer = input("Do you want to add a new To-Do item? (y/n) or exit to end:  ")
+        if user_sub_answer == "y":
+            file = open("to_do.txt","r",encoding="utf-8")
+            content = file.readlines()
+            for index ,item in enumerate(content) :
+               print(f" {index+1} - {item}")
+            file.close
+        elif user_answers =="exit":
+            break
+    elif user_answers == "exit" :
+        break
+print("thank you !! ")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 while True:
     print("exit to end ")
     user_answers = input("Do you want to add a new To-Do item? (y/n) or exit to end:  ")
     if user_answers == "y":
         user_item = input("Please type in your new To-Do item: ")
         to_do = open("to_do.txt", "a+", encoding="utf-8")
-        to_do.write(f"{[user_item] }")
+        to_do.write(user_item + "\n")
         to_do.close()
     elif user_answers == "n":
         user_answers2 = input("please add the new item , or exit to end ? : ")
@@ -23,3 +60,10 @@ while True:
         break
     else:
         print("your answer must be (y/n/exit)")
+
+
+# or 
+
+
+"""
+
